ff172d42fdd09de78a3f00e7698d95e40e1d055c
michelvn <lucaslepoisson@hotmail.com>
2019-12-11 16:45:18 +0100

    Fin View  login & register

--- Projet/app/src/main/java/com/example/mobilegenicotanciaux/LogInActivity.java
+++ Projet/app/src/main/java/com/example/mobilegenicotanciaux/LogInActivity.java
@@ -2,13 +2,36 @@ package com.example.mobilegenicotanciaux;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LogInActivity extends AppCompatActivity {
+
+    @BindView(R.id.leaf)
+    public ImageView leaf;
+    @BindView(R.id.button_register)
+    public Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        ButterKnife.bind(this);
+        Glide.with(LogInActivity.this).load(R.drawable.leaf).into(leaf);
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogInActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Projet/app/src/main/java/com/example/mobilegenicotanciaux/RegisterActivity.java
+++ Projet/app/src/main/java/com/example/mobilegenicotanciaux/RegisterActivity.java
@@ -3,12 +3,23 @@ package com.example.mobilegenicotanciaux;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    @BindView(R.id.leaf)
+    public ImageView leaf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        ButterKnife.bind(this);
+        Glide.with(RegisterActivity.this).load(R.drawable.leaf).into(leaf);
     }
 }
